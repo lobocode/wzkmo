d26e02496e050ef4a8671bed91e7f42a48f41ca6
Vitor Lobo <lobocode@gmail.com>
2022-08-10 19:54:57 -0300

    add env,homedir read yaml config file

--- cmd/kernel.go
+++ cmd/kernel.go
@@ -26,3 +26,39 @@ func Execute() {
 		os.Exit(1)
 	}
 }
+
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings in yaml file.
+	dir, _ := os.Getwd()
+	defaultConfig := filepath.Join(dir, ".wzkmo.yaml")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", defaultConfig, "config file")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".wzkmo" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".wzkmo")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
