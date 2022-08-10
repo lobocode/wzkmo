8ab0e05af3d6bdf9fa2e3897d1532b67fd91e00f
Vitor Lobo <lobocode@gmail.com>
2022-08-10 19:46:38 -0300

    add init kernel

---
+++ cmd/kernel.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "wzkmo",
+	Short: "Controle de recuo e autoping",
+	Long:  ``,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
